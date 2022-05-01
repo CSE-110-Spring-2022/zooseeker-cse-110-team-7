c5ba5a1ecdeaf090c1108547df28d491d9afc863
Steve-Morales <sam008@ucsd.edu>
2022-04-30 21:04:49 -0700

    integrated

---
+++ app/src/main/java/com/example/cse110lab5/TodoListAdapter.java
@@ -0,0 +1,103 @@
+package com.example.cse110lab5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class TodoListAdapter extends RecyclerView.Adapter <TodoListAdapter.ViewHolder>{
+    private List<TodoListItem> todoItems = Collections.emptyList();
+    private Consumer<TodoListItem> onCheckBoxClicked;
+    private Consumer<TodoListItem> onDeleteButtonClicked;
+    private BiConsumer<TodoListItem, String> onTextEditedHandler;
+
+    public void setTodoListItems (List newTodoItems) {
+        this.todoItems.clear();
+        this.todoItems = newTodoItems;
+        notifyDataSetChanged();
+    }
+
+    public void setOnCheckBoxClickedHandler(Consumer<TodoListItem> onCheckBoxClicked){
+        this.onCheckBoxClicked = onCheckBoxClicked;
+    }
+
+    public void setOnTextEditedHandler(BiConsumer<TodoListItem, String> onTextEdited){
+        this.onTextEditedHandler = onTextEdited;
+    }
+
+    public void setOnDeleteButtonClicked(Consumer<TodoListItem> onDeleteButtonClicked){
+        this.onDeleteButtonClicked = onDeleteButtonClicked;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext()) .inflate(R.layout.todo_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setTodoItem(todoItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return todoItems.size();
+    }
+
+    @Override
+    public long getItemId(int position) { return todoItems.get(position).id; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private final TextView textView;
+        private TodoListItem todoItem;
+        private final CheckBox todoCheck;//
+        private final TextView deleteBtn;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.todo_list_item);
+            this.todoCheck = itemView.findViewById(R.id.completed);//
+            this.deleteBtn = itemView.findViewById(R.id.delete_btn);
+
+            this.textView.setOnFocusChangeListener(((view, hasFocus) -> {
+                if(!hasFocus){
+                    onTextEditedHandler.accept(todoItem, textView.getText().toString());
+                }
+            }));
+
+            this.todoCheck.setOnClickListener(view -> {
+                if(onCheckBoxClicked == null){
+                    return;
+                }
+                onCheckBoxClicked.accept(todoItem);
+            });
+
+            this.deleteBtn.setOnClickListener(view -> {
+                if(onDeleteButtonClicked == null){ return; }
+                onDeleteButtonClicked.accept(todoItem);
+            });
+        }
+
+        public TodoListItem getTodoItem() { return todoItem; }
+
+        public void setTodoItem (TodoListItem todoItem) {
+            this.todoItem = todoItem;
+            this.textView.setText(todoItem.text);
+
+            this.todoCheck.setChecked(todoItem.completed);//
+        }
+    }
+}
